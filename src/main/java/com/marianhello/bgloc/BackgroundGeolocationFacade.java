ed8c7981939546a2cde5a00777e119278a886618
Suman Saurabh <sumansaurabh379@gmail.com>
2025-02-17 21:50:49 +0530

    fix permission error

--- src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
+++ src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
@@ -54,11 +54,11 @@ public class BackgroundGeolocationFacade {
     
     public static final String[] INITIALPERMISSIONS = new String[]{
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACTIVITY_RECOGNITION
+            Manifest.permission.ACCESS_FINE_LOCATION
     };
     public static final String[] BACKGROUNDLOCATIONPERMISSION = new String[]{
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            Manifest.permission.ACTIVITY_RECOGNITION
     };
 
     public static final String[] PERMISSIONS = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ? new String[]{
