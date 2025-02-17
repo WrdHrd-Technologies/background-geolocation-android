13bce6d52a61272f2f7c76312eddd1ba48e139dd
Suman Saurabh <sumansaurabh379@gmail.com>
2025-02-17 17:56:45 +0530

    Activity Permission

--- src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
+++ src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
@@ -54,7 +54,8 @@ public class BackgroundGeolocationFacade {
     
     public static final String[] INITIALPERMISSIONS = new String[]{
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACTIVITY_RECOGNITION
     };
     public static final String[] BACKGROUNDLOCATIONPERMISSION = new String[]{
             Manifest.permission.ACCESS_BACKGROUND_LOCATION
@@ -63,7 +64,8 @@ public class BackgroundGeolocationFacade {
     public static final String[] PERMISSIONS = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ? new String[]{
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            Manifest.permission.ACTIVITY_RECOGNITION
     }
     :
     new String[]{
