32465cee6b42fc24065e3d13084f887b7cea1090
Suman Saurabh <sumansaurabh379@gmail.com>
2025-06-13 15:48:19 +0530

    Consider accuracy while checking for geofence.

--- src/main/java/com/wrdhrd/geofence/GeofenceHelper.java
+++ src/main/java/com/wrdhrd/geofence/GeofenceHelper.java
@@ -19,6 +19,47 @@ public class GeofenceHelper {
         mGeofenceDAO = DAOFactory.createGeofenceDAO(context);
     }
 
+    public double distanceToSegment(double lat, double lon, double lat1, double lon1, double lat2, double lon2) {
+        double A = lat - lat1;
+        double B = lon - lon1;
+        double C = lat2 - lat1;
+        double D = lon2 - lon1;
+
+        double dot = A * C + B * D;
+        double lenSq = C * C + D * D;
+        double param = dot / lenSq;
+
+        double xx, yy;
+
+        if (param < 0 || (lat1 == lat2 && lon1 == lon2)) {
+            xx = lat1;
+            yy = lon1;
+        } else if (param > 1) {
+            xx = lat2;
+            yy = lon2;
+        } else {
+            xx = lat1 + param * C;
+            yy = lon1 + param * D;
+        }
+
+        double dx = lat - xx;
+        double dy = lon - yy;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    boolean isWithinAccuracy(double lat, double lon, List<double[]> polygonVertices, float accuracy) {
+        for (int i = 0; i < polygonVertices.size() - 1; i++) {
+            double[] vertex1 = polygonVertices.get(i);
+            double[] vertex2 = polygonVertices.get(i + 1);
+
+            double distance = distanceToSegment(lat, lon, vertex1[1], vertex1[0], vertex2[1], vertex2[0]);
+            if (distance <= accuracy) {
+                return true; // Location is within the buffer zone
+            }
+        }
+        return false;
+    }
+
     public boolean isPointInPolygon(double lat, double lon, List<double[]> polygon) {
         int intersectCount = 0;
         for (int i = 0; i < polygon.size(); i++) {
@@ -37,12 +78,35 @@ public class GeofenceHelper {
         // If intersect count is odd, point is inside the polygon
         return (intersectCount % 2) == 1;
     }
-    private boolean isPointInBoundingBox(BackgroundLocation location, Geofence geofence) {
-        double lat = location.getLatitude(),lon = location.getLongitude();
+    private boolean isPointInBoundingBox(double lat, double lon, Geofence geofence) {
         double minLat = geofence.getMinLatitude(), maxLat = geofence.getMaxLatitude(), minLon = geofence.getMinLongitude(),maxLon = geofence.getMaxLongitude();
 
         // Check if the point is within the bounding box
         return lat >= minLat && lat <= maxLat && lon >= minLon && lon <= maxLon;
+    }
+
+    private Geofence checkGeofence(double lat, double lon,float accuracy){
+        Geofence geofence = null;
+        Cursor cursor = mGeofenceDAO.getCursor();
+        while (cursor.moveToNext()) {
+          Geofence currGeofence = mGeofenceDAO.hydrate(cursor);
+
+          // First, check if the point is in the bounding box
+          if (isPointInBoundingBox(lat,lon, currGeofence)) {
+              // Then check if the point is inside the polygon
+              if (isPointInPolygon(lat, lon, currGeofence.getCoordinates())) {
+                 geofence = currGeofence;
+                 break;
+              }
+              if (isWithinAccuracy(lat, lon, currGeofence.getCoordinates(), accuracy)) {
+                geofence = currGeofence;
+                break;
+              }
+          }
+        }
+        cursor.close();
+
+        return geofence;
     }
 
     public BackgroundLocation checkGeofence(BackgroundLocation location){
@@ -53,27 +117,17 @@ public class GeofenceHelper {
         }
         //Check for the last geofence
         else if(lastGeofence != null){
-            if (isPointInBoundingBox(location,lastGeofence)) {
+            if (isPointInBoundingBox(location.getLatitude(),location.getLongitude(),lastGeofence)) {
                 if (isPointInPolygon(location.getLatitude(),location.getLongitude(),lastGeofence.getCoordinates())) {
+                    geofence = lastGeofence;
+                }
+                if (isWithinAccuracy(location.getLatitude(), location.getLongitude(), lastGeofence.getCoordinates(), location.getAccuracy())) {
                     geofence = lastGeofence;
                 }
             }
         }
         if(geofence == null) {
-            Cursor cursor = mGeofenceDAO.getCursor();
-            while (cursor.moveToNext()) {
-                Geofence currGeofence = mGeofenceDAO.hydrate(cursor);
-
-                // First, check if the point is in the bounding box
-                if (isPointInBoundingBox(location, currGeofence)) {
-                    // Then check if the point is inside the polygon
-                    if (isPointInPolygon(location.getLatitude(), location.getLongitude(), currGeofence.getCoordinates())) {
-                        geofence = currGeofence;
-                        break;
-                    }
-                }
-            }
-            cursor.close();
+            geofence = checkGeofence(location.getLatitude(), location.getLongitude(),location.getAccuracy());
         }
 
         if(geofence != null){
@@ -81,7 +135,6 @@ public class GeofenceHelper {
             location.setGeofenceId(geofence.getId());
         }
         lastGeofence = geofence;
-
         return location;
     }
 }
